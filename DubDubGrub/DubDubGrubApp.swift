b24d2966ab72103e788044dc087e7bd737b1f4bf
SylvainDruaux <sydru06@gmail.com>
2024-01-29 19:47:56 +0100

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Sylvain Druaux on 29/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
